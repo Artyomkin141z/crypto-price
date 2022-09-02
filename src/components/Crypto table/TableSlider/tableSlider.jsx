8e9f351ec84f3577d1a2674c1fae3eb095f56bd0
Artyomkin141z <gilimov2artyom@gmail.com>
2022-09-02 20:58:34 +0300

    table slider

--- src/components/Crypto table/TableSlider/tableSlider.jsx
+++ src/components/Crypto table/TableSlider/tableSlider.jsx
@@ -1,26 +1,120 @@
+import { Component } from 'react';
 import styles from './styles.module.css'
 
-const TableSlider = (props) => {
-    const countPages = Math.ceil(props.countCoin/20);
+class TableSlider extends Component{
+    countPages = Math.ceil(this.props.countCoin/20);
+    state = {
+        activePage: 1,
+    }
 
-    return(
-        <div className={styles.container}>
-            <div className={styles.slider}>
-                <button></button>
-                <div className={styles.pages}>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>7</div>
-                    <div>8</div>
+    printButtonPages(){
+        const pages = [];
+        for(let i = this.state.activePage - 4; i < this.state.activePage; i++){
+            let pageNumber = '';
+            if(i > 0) pageNumber = i;
+            pages.push(
+                <div className={styles.page}>
+                    {pageNumber}
                 </div>
-                <button></button>
+            );
+        }
+        pages.push(
+            <div className={styles.page + ' ' + styles.active}>
+                {this.state.activePage}
             </div>
-        </div>
-    );
+        );
+        let lastPage = this.state.activePage + 4 > this.countPages ? this.countPages : this.state.activePage + 4;
+        for(let i = this.state.activePage + 1; i <= lastPage; i++){         
+            pages.push(
+                <div className={styles.page}>
+                    {i}
+                </div>
+            );
+        }
+        return pages;
+    }
+    
+    render(){
+        return(
+            <div className={styles.container}>
+                <div className={styles.slider}>
+                    <button
+                        onClick={() => {      
+                            // this.state.activePage = activePage > 2 ? activePage-1 : 1;
+                            this.setState({activePage: this.state.activePage > 2 ? this.state.activePage - 1 : 1});
+                            this.forceUpdate();
+                            console.log(this.state.activePage);
+                        }}
+                    ></button>
+                    <div className={styles.pages}>
+                        {this.printButtonPages()}
+                    </div>
+                    <button
+                        onClick={() => {   
+                            this.setState({activePage: this.state.activePage < this.countPages - 1 ? this.state.activePage + 1 : this.countPages});
+                            this.forceUpdate();
+                            // activePage = activePage < countPages - 1 ? activePage+1 : countPages;
+                            console.log(this.state.activePage);
+                        }}             
+                    ></button>
+                </div>
+            </div>
+        );
+    }
 }
+
+// const TableSlider = (props) => {
+//     const countPages = Math.ceil(props.countCoin/20);
+//     let activePage = 1;
+
+//     function printButtonPages(countPages, activePage){
+//         const pages = [];
+//         for(let i = activePage - 4; i < activePage; i++){
+//             let pageNumber = '';
+//             if(i > 0) pageNumber = i;
+//             pages.push(
+//                 <div className={styles.page}>
+//                     {pageNumber}
+//                 </div>
+//             );
+//         }
+//         pages.push(
+//             <div className={styles.page + ' ' + styles.active}>
+//                 {activePage}
+//             </div>
+//         );
+//         let lastPage = activePage + 4 > countPages ? countPages : activePage + 4;
+//         for(let i = activePage + 1; i <= lastPage; i++){         
+//             pages.push(
+//                 <div className={styles.page}>
+//                     {i}
+//                 </div>
+//             );
+//         }
+//         return pages;
+//     }
+
+//     return(
+//         <div className={styles.container}>
+//             <div className={styles.slider}>
+//                 <button
+//                     onClick={() => {      
+//                         activePage = activePage > 2 ? activePage-1 : 1;
+//                         console.log(activePage);
+//                     }}
+//                 ></button>
+//                 <div className={styles.pages}>
+//                     {printButtonPages(countPages, activePage)}
+//                 </div>
+//                 <button
+//                     onClick={() => {   
+//                         activePage = activePage < countPages - 1 ? activePage+1 : countPages;
+//                         console.log(activePage);
+//                     }}             
+//                 ></button>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default TableSlider;
